8b9a583267f2700c3c159c28fcf859185998bb23
Arjun Singh Rajput <rajputarjun1708@protonmail.com>
2021-10-01 14:25:52 +0530

    Event Listner added

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const open_btn = document.querySelector('.open-btn')
 const close_btn = document.querySelector('.close-btn')
 const nav = document.querySelectorAll('.nav')
-
+window.addEventListener("click", () => {
+    console.log("You knocked?");
+  });
